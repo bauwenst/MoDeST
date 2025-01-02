bf96ebba5547747046f786e63e1217c7774567b5
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2025-01-02 17:43:20 +0100

    Added a first attempt at imputing missing MorphyNet decompositions on-the-fly.

--- src/modest/datasets/morphynet.py
+++ src/modest/datasets/morphynet.py
@@ -82,14 +82,75 @@ class MorphyNetDataset_Inflection(MorphyNetDataset[MorphyNetInflection]):
         super().__init__(language=language, subset=MorphynetSubset.INFLECTIONAL)
 
     def _generate(self, path: Path, **kwargs) -> Iterable[MorphyNetInflection]:
+        prev: MorphyNetInflection = None
         for parts in iterateTsv(path):
             lemma, word, tag, decomposition = parts
-            yield MorphyNetInflection(
+            curr = MorphyNetInflection(
                 word=word,
                 raw_morpheme_sequence=decomposition,
                 lemma=lemma,
                 lexical_tag=tag
             )
+            if curr.word.startswith("no "):  # Skip this while keeping the previous object ready.
+                continue
+
+            # If this is the first good word, put it in the chamber for now.
+            if prev is None:
+                prev = curr
+                continue
+
+            # Impute the previous decomp if it doesn't exist.
+            if prev.decompose() == tuple("-"):
+                try:
+                    print(f"Last word '{prev.word}' had no morphemes. Will be imputed by looking forwards at", curr.word, "->", curr.morphemes)
+                    self._imputeMorphemes(with_known_morphemes=curr, with_unknown_morphemes=prev)
+                    print("\t", prev.morphemes)
+                except:
+                    print(f"\tFAILED; {prev.word} removed from dataset.")
+                    prev = curr
+                    continue
+            elif curr.decompose() == tuple("-"):  # The fact that this is in an 'else' implies that prev has morphemes, which means even when this branch does nothing, you can output it.
+                try:
+                    print(f"Current word '{curr.word}' has no morphemes, but the previous word did. Will be imputed by looking backwards at", prev.word, "->", prev.morphemes)
+                    self._imputeMorphemes(with_known_morphemes=prev, with_unknown_morphemes=curr)
+                    print("\t", curr.morphemes)
+                except:
+                    print(f"\tFAILED; trying again soon.")
+                    # Still output prev.
+
+            # Output
+            yield prev
+            prev = curr
+
+        if prev is not None:
+            yield prev
+
+    def _imputeMorphemes(self, with_known_morphemes: MorphyNetInflection, with_unknown_morphemes: MorphyNetInflection):
+        known_morphemes = with_known_morphemes.decompose()
+        assert known_morphemes != tuple("-")
+        assert len(known_morphemes) == 2  # TODO: Needs to be loosened to allow e.g. inferring  'desleídme' -> ('desleid', 'me')  (where 'desleid' itself has a previous decomposition known)   from   desliámoslas -> ('desleír', 'amos', 'las'), where you find the stem by looking at only the first morph because all the inflection is done through suffixation.
+        assert with_known_morphemes.lemma == with_unknown_morphemes.lemma
+
+        # 1. Let Viterbi find the morph corresponding to the last morpheme of the next word.
+        last_morph_in_curr = with_known_morphemes.segment()[-1]
+
+        # 2. Trim that from that word to reveal a stem.
+        stem = with_known_morphemes.word.removesuffix(last_morph_in_curr)
+
+        # 3. The first word's suffix is whatever is after that stem.
+        uncertain = False
+        while stem and not with_unknown_morphemes.word.startswith(stem):
+            stem = stem[:-1]
+            uncertain = True
+        assert stem
+        if uncertain:
+            print("\tWARNING: removed only part of the found stem. May have removed too much stem.")
+
+        suffix = with_unknown_morphemes.word.removeprefix(stem)
+        if suffix:
+            with_unknown_morphemes.morphemes = known_morphemes[:-1] + (suffix,)
+        else:
+            with_unknown_morphemes.morphemes = known_morphemes[:-1]
 
 
 class MorphyNetDataset_Derivation(MorphyNetDataset[MorphyNetDerivation]):
